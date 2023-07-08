d412191904b2fe2fd2aa672982dc918b2f2e7ad3
kishanms999 <kishanms99@gmail.com>
2023-07-08 15:22:01 +0530

    login page

---
+++ login/login.js
@@ -0,0 +1,22 @@
+async function login(e){
+    try{
+        e.preventDefault();
+        let loginDetails={
+            email:e.target.emailid.value,
+            password:e.target.password.value
+        }
+        console.log(loginDetails)
+        const response = await axios.post("http://localhost:3000/user/login",loginDetails)
+            if(response.status === 200){
+                alert(response.data.message);
+                console.log(response.data)
+                localStorage.setItem('token',response.data.token)
+                window.location.href="../expense/index.html"
+            } else {
+                throw new Error(response.data.message)
+            }
+    } catch(err){
+        document.body.innerHTML+= `<div style="color:red;">${err}</div>`;
+    }
+}
+
